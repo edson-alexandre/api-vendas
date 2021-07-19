d186ed7f5deb9cf35479949d48a7e222c493b1ba
Edson Alexandre <edson.ass@gmail.com>
2021-07-19 12:48:56 -0300

    Criação do projeto

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,42 @@
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../typeorm/repositories/ProductsReposiroty';
+import Product from '../typeorm/entities/Product';
+import AppError from '../../../shared/errors/AppError';
+
+interface IRequest {
+    id: string;
+    name: string;
+    price: number;
+    quantity: number;
+}
+class UpdateProductService {
+    public async execute({
+        id,
+        name,
+        price,
+        quantity,
+    }: IRequest): Promise<Product> {
+        const productsRepository = getCustomRepository(ProductRepository);
+        const product = await productsRepository.findOne(id);
+
+        if (!product) {
+            throw new AppError('Product not found');
+        }
+
+        const productExists = await productsRepository.findByName(name);
+
+        product.name = name;
+        product.price = price;
+        product.price = price;
+
+        if (productExists) {
+            throw new AppError('There is already a product with this name');
+        }
+
+        await productsRepository.save(product);
+
+        return product;
+    }
+}
+
+export default UpdateProductService;
---
+++ src/modules/users/typeorm/entites/User.ts
@@ -0,0 +1,33 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    avatar?: string;
+
+    @CreateDateColumn()
+    create_At: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
+
+export default User;
